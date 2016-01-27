b96b55cab656799f4993964ea030f731e3057e73
Niksanllp <niksan.llp@gmail.com>
2016-01-27 15:33:02 -0500

    Initial Commit

---
+++ improvH/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  improvH
+//
+//  Created by Sandeep Singh on 1/27/16.
+//  Copyright © 2016 Niksan LLP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
